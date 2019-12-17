df14e593ecbe2544f558b4291947ca88cb3ec9e4
RusherRG <rushang101@gmail.com>
2019-12-18 01:33:00 +0530

    Mongoose rescued hell

--- hell/routes/problemStatement.js
+++ hell/routes/problemStatement.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ProblemStatement = require('./models/problemStatement.model')
+const ProblemStatement = require('../models/problemStatement.model')
 const router = express.Router();
 
 // const db = require('./dbconnection');
