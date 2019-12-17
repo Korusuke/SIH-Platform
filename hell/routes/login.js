c7e55eb9b79ab97f665ba5e7c3674a7097ac76b6
RusherRG <rushang101@gmail.com>
2019-12-18 03:22:36 +0530

    Login and PS Scheem

--- hell/routes/login.js
+++ hell/routes/login.js
@@ -1,49 +1,86 @@
 const express = require('express');
+const otpGenerator = require('otp-generator');
 const router = express.Router();
 
 router.post('/login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    .find({email, password})
-    if () {
-        res.json({status: 'success'});
-    } else {
-        res.json({status: 'failed', message: 'Invalid email-password pair'});
-    }
+    User.find({email, password})
+        .then(user => {
+            if(user)
+                res.status(200).json({loggedin: true});
+            else 
+                res.status(200).json({loggedin: false});       
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.post('/verify', (req, res) => {
     const email = req.body.email;
     const otp = req.body.otp;
-    try {
-        user = .find({email});
-        if (user.otp==otp){
-            res.json({status: 'success'});
-        } else {
-            res.json({status: 'failed', message: 'wrong message'});
-        }
-    } catch(error) {
-        return res.json({status: "failed", message: 'Error occurred'});
-    }
+    User.find({email, otp})
+        .then(user => {
+            if(user)
+                res.status(200).json({verified: true});
+            else 
+                res.status(200).json({verified: false});       
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 })
 
 router.post('/signup', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    try {
-        .find({email});
-        if () {
-            const otp = "otp";
-            .insert({email, password, otp});
-        
-            res.json({status: 'success', otp: otp});
-        } else {
-            res.json({status: 'failed', message: 'User already exists'});
-        }
-        
-    } catch (error) {
-        return res.json({status: "failed", message: 'Error occurred'});
-    }
+    const otp = otpGenerator.generate(6, { specialChars: false });
+    
+    const newUser = new User({email, password, otp});
+
+    newUser.save()
+        .then(() => {
+            const emailId = process.env.EMAIL;
+            const password = process.env.PASSWORD; 
+            var transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth:{
+                    user: emailId,
+                    pass: password
+                },
+                port: 465,
+                secureConnection: true,
+                });
+            
+            const team = "Team_Probably";
+            var mailOptions = {
+                from: emailId,
+                to: member,
+                subject: "Invitation code for SIH internal hackathon",
+                html: `<html>
+                <body>
+                    <p>Hey,
+                    <br>You have been invited by ${sender} to join the team ${team}.<br>
+                    <br>Here is the code to join the team: ${code}</br>
+                    </p>
+                </body>
+                </html>`
+            }
+
+            transporter.sendMail(mailOptions, function(err){
+                if(err){
+                    res.json({
+                        'msg':'Failed to send mail'
+                    });
+                    throw(err);
+                }
+                console.log('Mail sent succesfully');
+                res.json({
+                    msg: 'Check your mail for otp'
+                });
+            });
+            transporter.close();
+
+            res.json('User added!')
+        })
+        .catch(err => res.status(400).json('Error: ' + err))
 })
 
 module.exports = router;
--- hell/routes/problemStatement.js
+++ hell/routes/problemStatement.js
@@ -2,14 +2,10 @@ const express = require('express');
 const ProblemStatement = require('../models/problemStatement.model')
 const router = express.Router();
 
-// const db = require('./dbconnection');
-
 router.get('/', (req, res) => {
-    console.log(ProblemStatement);
     ProblemStatement.find()
         .then(ProblemStatement => res.json(ProblemStatement))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
-
