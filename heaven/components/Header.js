117cf42caecf110093aec5c464bad273cc350f74
Akshay Padte <akshaypadte99@gmail.com>
2019-12-17 15:36:11 +0530

    Index page skeleton

--- heaven/components/Header.js
+++ heaven/components/Header.js
@@ -1,8 +1,55 @@
 import React from 'react';
+import {AppBar, Toolbar,Grid} from '@material-ui/core/';
 
+import Center from 'react-center'
 
 export default function Header(){
+
+    let styles = {
+        center:{
+            height: '100%'
+        },
+        image:{
+            backgroundSize: 'contain',
+            width: '100%'
+        }
+
+    }
     return (
-        <h1>Header</h1>
+        <div>
+            <AppBar position="static" style={
+                {
+                    backgroundColor: 'white',
+                    color: 'black'
+                }
+            } >
+                <Toolbar>
+                    <Grid 
+                    container 
+                    direction="row"
+                    spacing={3}
+                    >
+                        <Grid item xs={2} sm={1}>
+                            <Center style={styles.center}>
+                            <img src="/assets/images/somaiya.png" style={styles.image} />
+                            </Center>
+                        </Grid>
+                        <Grid item xs={2} sm={1}>
+                            <Center style={styles.center}>
+                                <img src="/assets/images/sihlogo.png" style={styles.image} />
+                            </Center>
+                        </Grid>
+                        <Grid item xs={8} sm={10}>
+                            <Center>
+                                <h1>Internal Hackathon</h1>
+                            </Center>
+                        </Grid>
+                        
+                            
+                    </Grid>
+                
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
