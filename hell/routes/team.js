93bc16f5112eca3706cdd55b8f866274b91ad602
Aditya <aditya10699@gmail.com>
2019-12-18 03:15:12 +0530

    Add routes and endpoints To create/join teams and send invitation mail to other members

--- hell/routes/team.js
+++ hell/routes/team.js
@@ -1,24 +1,71 @@
 const express = require('express');
 const router = express.Router();
 var otpGenerator = require('otp-generator');    
-const { uuidv4 } = require('uuidv4');
+const { uuid } = require('uuidv4');
+const Team = require('../models/team.model');
+const User = require('../models/user.model');
 
-const db = require('./dbconnection');
+router.post('/create', (req, res)=>{
+  const { TeamName } = req.body;
+  Team.findOne({'TeamName': TeamName}, (err, result)=>{
+    if(err){
+      console.log(err);
+      res.send(500);
+      return;
+    }
+    if(result){
+      res.send('Team name already taken');
+      return;
+    }
+    const Leader = req.body.sender;
+    let Members = [Leader];
+    InviteCode = otpGenerator.generate(6, { specialChars: false });
+    TeamId = uuid(); 
+    const doc = {
+      TeamName,
+      Leader,
+      Members,
+      InviteCode,
+      TeamId
+    };
+    const team1 = new Team(doc);
+    team1.save((err, result)=>{
+      if(err){
+        return console.log(err);
+      }
+      console.log("Saved", result);
+      res.json({
+        'msg': 'Saved document succesfully'
+      });
+    });
+  });
+});
 
-router.post('/', (req, res)=>{
-  const { teamName } = req.body;
-  const leader = req.body.sender;
-  members = [leader];
-  inviteCode = otpGenerator.generate(6, { specialChars: false });
-  tid = uuidv4(); 
-  const doc = {
-    teamName,
-    leader,
-    members,
-    inviteCode,
-    tid
-  };
-  res.json({msg: "Sup dawg"});
+router.post('/join', (req, res)=>{
+  const { sender, InviteCode } = req.body;
+  Team.findOne({'InviteCode': InviteCode}, (err, result)=>{
+    if(err){
+      res.send(500);
+      return;
+    }
+    if(!result){
+      res.send('Check your invite code again');
+      return;
+    }
+    console.log(result);
+    result.Members.push(sender);
+    result.save();
+    res.json({
+      msg:'Hello there'
+    });
+    User.findOne({'email': sender}, (err, result)=>{
+      if(err){
+        res.send(500);
+        return;
+      }
+      console.log(result);
+    });
+  });
 });
 
 module.exports = router;
