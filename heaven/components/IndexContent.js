0973a8e50f0eccc1505f4c6479c3ba00f49ce76b
Akshay Padte <akshaypadte99@gmail.com>
2019-12-17 16:17:41 +0530

    resolve merge

--- heaven/components/IndexContent.js
+++ heaven/components/IndexContent.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 import Login from '../components/Login'
 import Timeline from '../components/Timeline';
-import { Grid , Paper,  } from '@material-ui/core';
+import { Grid , Paper,  Box, Container} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -28,17 +28,22 @@ const useStyles = makeStyles(theme => ({
 export default function IndexContent()
 {
     return(
+        <Container maxWidth="xl" style={{paddingTop:'100px'}}>
         <Grid 
             container
             direction="row"
+            spacing={3}
         >
-            <Grid item md={6} xs={12}>
-                <Timeline />
+            <Grid item md={6} xs={12} style={{padding:'10px;'}}>
+                <Box boxShadow={3} style={{background:'white'}}>
+                    <Timeline />
+                </Box>
             </Grid>
             <Grid item md={6} xs={12}>
                 <Login />
             </Grid>
 
         </Grid>
+        </Container>
     )
 }
