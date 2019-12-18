8193111b5e5f54a8ec2349c54bed55ed51560d35
Akshay Padte <akshaypadte99@gmail.com>
2019-12-18 08:56:35 +0530

    Integrate upper

--- heaven/pages/profileMember.js
+++ heaven/pages/profileMember.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+import Header from '../components/Header'
 import Profile from '../components/Profile';
+
+import {Container, Paper} from '@material-ui/core' 
+
+import '../styles/index.css'
 
 export default function ProfileMember(){
     return(
         <div>
-            < Profile />
+        <Header/>
+        <Container maxWidth="xl">
+        <Paper style={{
+            padding: '40px'
+        }}>
+        < Profile />
+        </Paper>
+        </Container>
         </div>
     );
 }
