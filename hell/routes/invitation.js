e9c260a7de0551f91ab6690e6f3b0a200d477e4a
Aditya <aditya10699@gmail.com>
2019-12-19 14:22:11 +0530

    Minor changes + refactoring
    - Change routing for /team/invite
    - Fix issue in redis code
    - Move redis client creation and token verification to seperate file
    - Implement previously incomplete code in invitation.js

--- hell/routes/invitation.js
+++ hell/routes/invitation.js
@@ -2,39 +2,16 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 var otpGenerator = require('otp-generator');    
+const Team = require('../models/team.model');
+const jwt = require('jsonwebtoken');
 
-async function verifyToken(req, res, next) {
-  const token = req.cookies.token;
-  if(!token){
-    res.json({
-      'msg': 'Token not present'
-    });
-  }
-  blacklistedTokens = await client.lrange('blacklistedTokens',0,-1);
-  if(token in blacklistedTokens){
-    res.json({
-      "msg":"Token invalidated, please sign in again",
-    });
-    return;
-  } 
-  jwt.verify(token, privateKey, (err) => {
-    if (err) {
-      res.json({'msg': 'Token expired'});
-    }
-    else{
-      req.token = token;
-      next();
-    }
-  });
-}
-
-router.post('/', verifyToken, (req,res)=>{
+router.post('/', (req,res)=>{
   const emailId = process.env.EMAIL;
   const password = process.env.PASSWORD; 
+  // person to be invited
   var member = req.body.email;
-  const decodedData = jwt.decode(req.token);
-  let sender = decodedData.Email.slice(-11);
-  sender = sender.replace(".", " ");
+  // person sending invitation;
+  let sender = (jwt.decode(req.token))?jwt.decode(req.token).Email.slice(-11):req.body.Email;
   var transporter = nodemailer.createTransport({
   service: "gmail",
   auth:{
@@ -45,34 +22,43 @@ router.post('/', verifyToken, (req,res)=>{
   secureConnection: true,
   });    
   let code = otpGenerator.generate(6, { specialChars: false });
-  // Fetch from db later
-  const team = "Team_Probably";
-  var mailOptions = {
-    from: emailId,
-    to: member,
-    subject: "Invitation code for SIH internal hackathon",
-    html: `<html>
-    <body>
-        <p>Hey,
-        <br>You have been invited by ${sender} to join the team ${team}.<br>
-        <br>Here is the code to join the team: ${code}</br>
-        </p>
-    </body>
-    </html>`
-  }
-  transporter.sendMail(mailOptions, function(err){
+  console.log(sender);
+  Team.find({members: [sender]}, (err, doc)=>{
     if(err){
-      res.json({
-          'msg':'Failed to send mail'
-      });
-      throw(err);
+      console.log(err);
+      res.sendStatus(500);
+      return;
     }
-    console.log('Mail sent succesfully');
-    res.json({
-        msg: 'Check your mail for otp'
+    console.log(doc);
+    sender = sender.replace(".", " ");
+    let teamName = doc.teamName;
+    var mailOptions = {
+      from: emailId,
+      to: member,
+      subject: "Invitation code for SIH internal hackathon",
+      html: `<html>
+      <body>
+          <p>Hey,
+          <br>You have been invited by ${sender} to join the team ${teamName}.<br>
+          <br>Here is the code to join the team: ${code}</br>
+          </p>
+      </body>
+      </html>`
+    }
+    transporter.sendMail(mailOptions, function(err){
+      if(err){
+        res.json({
+            'msg':'Failed to send mail'
+        });
+        throw(err);
+      }
+      console.log('Mail sent succesfully');
+      res.json({
+          msg: 'Check your mail for otp'
+      });
     });
+    transporter.close();
   });
-  transporter.close();
 });
 
 module.exports = router;
