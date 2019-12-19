90c7373d2cf87a8467277d0a55398f5cef07b5ba
RusherRG <rushang101@gmail.com>
2019-12-19 23:16:59 +0530

    Added delete collection endpoint

---
+++ hell/routes/delete.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+require('dotenv').config();
+
+router.delete('/', (req, res)=>{
+    const { email, key, col } = req.body;
+    if (email != process.env.EMAIL && key != process.env.KEY)
+        return res.json({
+            'status': 'failure', 
+            'msg': 'Invalid Credentials'
+        })
+    if (!col)
+        return res.json({
+            'status': 'failure',
+            'msg': 'Invalid Fields',
+        })
+    try {
+        var collection = require(`../models/${col}.model`)
+        collection.deleteMany({})
+            .then(() => {
+                res.json({
+                    'status': 'success',
+                    'msg': 'Collection deleted successfully'
+                })
+            })
+    } catch(err) {
+        return res.json({
+            'status': 'failure',
+            'msg': 'Delete collection failed'
+        })
+    }
+})
