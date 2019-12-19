b084f69b06ad37b7cf83fb0abc8cb8a517397745
Akshay Padte <akshaypadte99@gmail.com>
2019-12-19 17:25:15 +0530

    server

--- heaven/server.js
+++ heaven/server.js
@@ -1,7 +1,7 @@
 const next = require('next')
 const express = require('express')
 const axios = require('axios')
-
+const cookieParser = require('cookie-parser');
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -10,14 +10,17 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
     const server = express()
 
+    server.use(cookieParser());
+
     server.get('/', (req, res) => {
-        if(req.cookie && req.cookie.token)
+        if(req.cookies && req.cookies.token)
             return res.redirect('/profile')
         return app.render(req, res, '/index', req.query)
     })
 
     server.get('/profile', (req, res) => {
-        if(req.cookie && req.cookie.token)
+        console.log(req)
+        if(req.cookies && req.cookies.token)
             return app.render(req, res, '/profileMember', req.query)
         return res.redirect('/')
     })
