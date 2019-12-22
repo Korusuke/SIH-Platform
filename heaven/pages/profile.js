496c1cdef19ffda0c0e3696475ca2f72aa3b2cd5
hetzz <hetal.kuvadia@somaiya.edu>
2019-12-22 15:40:37 +0530

    Chaand stuff

--- heaven/pages/profile.js
+++ heaven/pages/profile.js
@@ -10,12 +10,55 @@ import Bottomcardfirst from '../components/bottomcardfirst';
 import Bottomcardconf from '../components/bottomcardconf';
 import BottomCardMerge from '../components/BottomCardMerge'
 import {Container, Paper} from '@material-ui/core'
-
+import { useTheme, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Chaand from '../components/chaand';
 import '../styles/index.css'
 
-export default function ProfileMember(){
-    return(
+export default class extends React.Component{
+    theme = {
+        dark: {
+            background: '#121212',
+            text: '#FFFFFF',
+        },
+        light: {
+            background: '#FCFCFC',
+            text: '#000000',
+        }
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            theme: 'light'
+        }
+        this.handler = this.handler.bind(this)
+    }
+
+    handler() {
+        if (this.state.theme == 'light') {
+            this.setState({
+                theme: 'dark'
+            })
+        } else {
+            this.setState({
+                theme: 'light'
+            })
+        }
+    }
+    render(){
+        const customtheme = createMuiTheme({
+            palette: {
+                type: this.state.theme == 'light' ? 'light' : 'dark',
+            },
+        });
+        let curtheme = this.state.theme == 'light' ? this.theme.light : this.theme.dark;
+
+
+        return(
+            <ThemeProvider theme={customtheme}>
         <div>
+        <Chaand handler={this.handler} chaand={this.state.theme == 'light' ? 1 : 0} />
+        <div id="content-wrap" style={{ backgroundColor: `${curtheme.background}`, color: `${curtheme.text}` }}>
         <Header/>
         <Container maxWidth="lg">
         <Paper style={{
@@ -29,5 +72,8 @@ export default function ProfileMember(){
             <BottomCardMerge url="http://localhost:8080"/>
         </Container>
         </div>
-    );
+        </div>
+        </ThemeProvider>
+    )
+    };
 }
