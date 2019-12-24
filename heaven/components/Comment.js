0bd1ed72dc8a6affe9a1ec66a93e49345199cd5d
Akshay Padte <akshaypadte99@gmail.com>
2019-12-24 15:57:32 +0530

    Minor bug fixes

--- heaven/components/Comment.js
+++ heaven/components/Comment.js
@@ -39,7 +39,7 @@ export default function Comment(props)
                         </Grid>
                     </Grid>
                 </div>
-                <div style={{background: 'white', minHeight:'20px', padding: '15px'}}>
+                <div style={{minHeight:'20px', padding: '15px'}}>
                 <div style={{whiteSpace: 'pre-wrap'}}>
                     {props.message}
                 </div>
